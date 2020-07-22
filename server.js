0b5fbf3abfa51399adddbaab7f0ee6dfac066f5c
Taufiq <61982529+Uplus1D70F@users.noreply.github.com>
2020-07-22 18:27:00 +0400

    added update client feature to api

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  return res.status(200).send({'message': 'SHIPTIVITY API. Read documentation to see API docs'});
+  return res.status(200).send({ 'message': 'SHIPTIVITY API. Read documentation to see API docs' });
 });
 
 // We are keeping one connection alive for the rest of the life application for simplicity
@@ -26,8 +26,8 @@ const validateId = (id) => {
     return {
       valid: false,
       messageObj: {
-      'message': 'Invalid id provided.',
-      'long_message': 'Id can only be integer.',
+        'message': 'Invalid id provided.',
+        'long_message': 'Id can only be integer.',
       },
     };
   }
@@ -36,8 +36,8 @@ const validateId = (id) => {
     return {
       valid: false,
       messageObj: {
-      'message': 'Invalid id provided.',
-      'long_message': 'Cannot find client with that id.',
+        'message': 'Invalid id provided.',
+        'long_message': 'Cannot find client with that id.',
       },
     };
   }
@@ -51,12 +51,12 @@ const validateId = (id) => {
  * @param {any} priority
  */
 const validatePriority = (priority) => {
-  if (Number.isNaN(priority)) {
+  if (Number.isNaN(priority) && priority) {
     return {
       valid: false,
       messageObj: {
-      'message': 'Invalid priority provided.',
-      'long_message': 'Priority can only be positive integer.',
+        'message': 'Invalid priority provided.',
+        'long_message': 'Priority can only be positive integer.',
       },
     };
   }
@@ -64,6 +64,36 @@ const validatePriority = (priority) => {
     valid: true,
   }
 }
+
+const resetDatabaseStatements = `DROP TABLE clients;
+CREATE TABLE IF NOT EXISTS clients ( id INTEGER PRIMARY KEY, name TEXT NOT NULL, description TEXT, status TEXT, priority INTEGER );
+insert into clients values(1,"Stark, White and Abbott", "Cloned Optimal Architecture", "in-progress", 1);
+insert into clients values(2,"Wiza LLC", "Exclusive Bandwidth-Monitored Implementation", "complete", 1);
+insert into clients values(3,"Nolan LLC", "Vision-Oriented 4Thgeneration Graphicaluserinterface", "backlog", 1);
+insert into clients values(4,"Thompson PLC", "Streamlined Regional Knowledgeuser", "in-progress", 2);
+insert into clients values(5,"Walker-Williamson", "Team-Oriented 6Thgeneration Matrix", "in-progress", 3);
+insert into clients values(6,"Boehm and Sons", "Automated Systematic Paradigm", "backlog", 2);
+insert into clients values(7,"Runolfsson, Hegmann and Block", "Integrated Transitional Strategy", "backlog", 3);
+insert into clients values(8,"Schumm-Labadie", "Operative Heuristic Challenge", "backlog", 4);
+insert into clients values(9,"Kohler Group", "Re-Contextualized Multi-Tasking Attitude", "backlog", 5);
+insert into clients values(10,"Romaguera Inc", "Managed Foreground Toolset", "backlog", 6);
+insert into clients values(11,"Reilly-King", "Future-Proofed Interactive Toolset", "complete", 2);
+insert into clients values(12,"Emard, Champlin and Runolfsdottir", "Devolved Needs-Based Capability", "backlog", 7);
+insert into clients values(13,"Fritsch, Cronin and Wolff", "Open-Source 3Rdgeneration Website", "complete", 3);
+insert into clients values(14,"Borer LLC", "Profit-Focused Incremental Orchestration", "backlog", 8);
+insert into clients values(15,"Emmerich-Ankunding", "User-Centric Stable Extranet", "in-progress", 4);
+insert into clients values(16,"Willms-Abbott", "Progressive Bandwidth-Monitored Access", "in-progress", 5);
+insert into clients values(17,"Brekke PLC", "Intuitive User-Facing Customerloyalty", "complete", 4);
+insert into clients values(18,"Bins, Toy and Klocko", "Integrated Assymetric Software", "backlog", 9);
+insert into clients values(19,"Hodkiewicz-Hayes", "Programmable Systematic Securedline", "backlog", 10);
+insert into clients values(20,"Murphy, Lang and Ferry", "Organized Explicit Access", "backlog", 11);`.split(/\r?\n/).map(
+  sql => db.prepare(sql)
+)
+
+const resetDatabase = db.transaction(() => {
+  for (const stmt of resetDatabaseStatements) stmt.run()
+})
+
 
 /**
  * Get all of the clients. Optional filter 'status'
@@ -92,7 +122,7 @@ app.get('/api/v1/clients', (req, res) => {
  * GET /api/v1/clients/{client_id} - get client by id
  */
 app.get('/api/v1/clients/:id', (req, res) => {
-  const id = parseInt(req.params.id , 10);
+  const id = parseInt(req.params.id, 10);
   const { valid, messageObj } = validateId(id);
   if (!valid) {
     res.status(400).send(messageObj);
@@ -115,7 +145,7 @@ app.get('/api/v1/clients/:id', (req, res) => {
  *
  */
 app.put('/api/v1/clients/:id', (req, res) => {
-  const id = parseInt(req.params.id , 10);
+  const id = parseInt(req.params.id, 10);
   const { valid, messageObj } = validateId(id);
   if (!valid) {
     res.status(400).send(messageObj);
@@ -123,12 +153,70 @@ app.put('/api/v1/clients/:id', (req, res) => {
 
   let { status, priority } = req.body;
   let clients = db.prepare('select * from clients').all();
-  const client = clients.find(client => client.id === id);
+  const originalClient = clients.find(client => client.id === id);
 
   /* ---------- Update code below ----------*/
+  let updatedClient = { ...originalClient}
+  if (status) {
+    if (status !== 'backlog' && status !== 'in-progress' && status !== 'complete') {  
+      return res.status(400).send({
+        'message': 'Invalid status provided.',
+        'long_message': 'Status can only be one of the following: [backlog | in-progress | complete].',
+      });
+    }
+    updatedClient.status = status
+  } else {
+    status = originalClient.status
+  }
 
+  if (priority) {
+    const { valid, messageObj } = validatePriority(priority)
+    if (!valid) return res.status(400).send(messageObj);
+    if (originalClient.status === status) {
+      if (originalClient.priority === priority) return
+      clients.map(client => {
+        if (client.status !== status) return
+        if (priority <= client.priority < originalClient.priority) {
+          client.priority = client.priority + 1
+        } else if (priority >= client.priority > originalClient.priority) {
+          client.priority = client.priority - 1
+        }
+      })
+    } else {
+      clients.map(client => {
+        if (client.status === originalClient.status) {
+          if (client.priority > originalClient.priority) {
+            client.priority = client.priority - 1
+          }
+        } else if (client.status === status) {
+          if (client.priority >= priority) {
+            client.priority = client.priority + 1
+          }
+        }
+      })
+    }
+  } else {
+    if (status = originalClient.status) return
+    priority = clients.filter(client => client !== status).length
+  }
+  updatedClient.priority = priority
+  clients = clients.map(client => {
+    return client.id === id ? updatedClient : client
+  })
 
+  // clients array is correct
 
+  const updateClient = db.prepare(
+    'UPDATE clients SET status = ?, priority = ? WHERE id = ?'
+  )
+  const updateAllClients = clients => {
+    clients.forEach((client) => {
+      updateClient.run( client.status, client.priority, client.id )
+    })
+  }
+
+  updateAllClients(clients)
+  
   return res.status(200).send(clients);
 });
 
