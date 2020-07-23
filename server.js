095ced6bc9cb4932fe7e78b7c346b6b13e9ab7fa
Taufiq <61982529+Uplus1D70F@users.noreply.github.com>
2020-07-23 17:34:17 +0400

    added comments and cleaned up code

--- server.js
+++ server.js
@@ -65,37 +65,6 @@ const validatePriority = (priority) => {
   }
 }
 
-const resetDatabaseStatements = `DROP TABLE clients;
-CREATE TABLE IF NOT EXISTS clients ( id INTEGER PRIMARY KEY, name TEXT NOT NULL, description TEXT, status TEXT, priority INTEGER );
-insert into clients values(1,"Stark, White and Abbott", "Cloned Optimal Architecture", "in-progress", 1);
-insert into clients values(2,"Wiza LLC", "Exclusive Bandwidth-Monitored Implementation", "complete", 1);
-insert into clients values(3,"Nolan LLC", "Vision-Oriented 4Thgeneration Graphicaluserinterface", "backlog", 1);
-insert into clients values(4,"Thompson PLC", "Streamlined Regional Knowledgeuser", "in-progress", 2);
-insert into clients values(5,"Walker-Williamson", "Team-Oriented 6Thgeneration Matrix", "in-progress", 3);
-insert into clients values(6,"Boehm and Sons", "Automated Systematic Paradigm", "backlog", 2);
-insert into clients values(7,"Runolfsson, Hegmann and Block", "Integrated Transitional Strategy", "backlog", 3);
-insert into clients values(8,"Schumm-Labadie", "Operative Heuristic Challenge", "backlog", 4);
-insert into clients values(9,"Kohler Group", "Re-Contextualized Multi-Tasking Attitude", "backlog", 5);
-insert into clients values(10,"Romaguera Inc", "Managed Foreground Toolset", "backlog", 6);
-insert into clients values(11,"Reilly-King", "Future-Proofed Interactive Toolset", "complete", 2);
-insert into clients values(12,"Emard, Champlin and Runolfsdottir", "Devolved Needs-Based Capability", "backlog", 7);
-insert into clients values(13,"Fritsch, Cronin and Wolff", "Open-Source 3Rdgeneration Website", "complete", 3);
-insert into clients values(14,"Borer LLC", "Profit-Focused Incremental Orchestration", "backlog", 8);
-insert into clients values(15,"Emmerich-Ankunding", "User-Centric Stable Extranet", "in-progress", 4);
-insert into clients values(16,"Willms-Abbott", "Progressive Bandwidth-Monitored Access", "in-progress", 5);
-insert into clients values(17,"Brekke PLC", "Intuitive User-Facing Customerloyalty", "complete", 4);
-insert into clients values(18,"Bins, Toy and Klocko", "Integrated Assymetric Software", "backlog", 9);
-insert into clients values(19,"Hodkiewicz-Hayes", "Programmable Systematic Securedline", "backlog", 10);
-insert into clients values(20,"Murphy, Lang and Ferry", "Organized Explicit Access", "backlog", 11);`.split(/\r?\n/).map(
-  sql => db.prepare(sql)
-)
-
-const resetDatabase = db.transaction(() => {
-  for (const stmt of resetDatabaseStatements) stmt.run()
-})
-
-
-
 /**
  * Get all of the clients. Optional filter 'status'
  * GET /api/v1/clients?status={status} - list all clients, optional parameter status: 'backlog' | 'in-progress' | 'complete'
@@ -146,31 +115,28 @@ app.get('/api/v1/clients/:id', (req, res) => {
  *
  */
 app.put('/api/v1/clients/:id', (req, res) => {
-  resetDatabase()
   const id = parseInt(req.params.id, 10);
-  let valid, messageObj 
+  let valid, messageObj
   ({ valid, messageObj } = validateId(id));
-  if (!valid) {
-    res.status(400).send(messageObj);
-  }
+  if (!valid) res.status(400).send(messageObj)
 
   let { status, priority } = req.body;
   let clients = db.prepare('select * from clients').all();
   const originalClient = clients.find(client => client.id === id);
 
-  /* ---------- Update code below ----------*/
-  let updatedClient = { ...originalClient}
   if (status) {
-    if (status !== 'backlog' && status !== 'in-progress' && status !== 'complete') {  
+    if (status !== 'backlog' && status !== 'in-progress' && status !== 'complete') {
       return res.status(400).send({
         'message': 'Invalid status provided.',
         'long_message': 'Status can only be one of the following: [backlog | in-progress | complete].',
       });
     }
-    updatedClient.status = status
   } else {
     status = originalClient.status
   }
+
+  // if no changes made, clients sent with no update to database
+
   const maxPriority = clients.filter(client => client.status === status).length + 1
   if (!priority) {
     if (originalClient.status === status) return res.status(200).send(clients)
@@ -180,11 +146,11 @@ app.put('/api/v1/clients/:id', (req, res) => {
   ({ valid, messageObj } = validatePriority(priority))
   if (!valid) return res.status(400).send(messageObj);
   if (originalClient.status === status) {
-    if (originalClient.priority === priority) {
-      return res.status(200).send(clients)
-    } 
+    if (originalClient.priority === priority) return res.status(200).send(clients)
+    // mapping clients to reflect shifting of cards
+    // this is shifting between a single swimlane
     clients.map(client => {
-      if (client.status !== status) return 
+      if (client.status !== status) return
       if (priority <= client.priority < originalClient.priority) {
         client.priority = client.priority + 1
       } else if (priority >= client.priority > originalClient.priority) {
@@ -192,38 +158,46 @@ app.put('/api/v1/clients/:id', (req, res) => {
       }
     })
   } else {
+    // mapping clients to reflect shifting of cards
+    // this is shifting between two different swimlanes
     clients.map(client => {
       if (client.status === originalClient.status) {
-        if (client.priority > originalClient.priority) {
+        // swimlane from where card is being picked
+        if (client.priority >= originalClient.priority) {
           client.priority = client.priority - 1
         }
       } else if (client.status === status) {
+        // swimlane where card is being placed
         if (client.priority >= priority) {
           client.priority = client.priority + 1
         }
       }
     })
   }
-
+  // fixing priority of exceeded size
   if (priority > maxPriority) priority = maxPriority
 
-  updatedClient.priority = priority
+  const updatedClient = {
+    ...originalClient,
+    status: status, 
+    priority: priority
+  }
+  // updating moved client in clients
   clients = clients.map(client => {
     return client.id === id ? updatedClient : client
   })
-  // clients array is set
 
+  // creating function for updating database
   const updateClient = db.prepare(
     'UPDATE clients SET status = ?, priority = ? WHERE id = ?'
   )
   const updateAllClients = clients => {
     clients.forEach((client) => {
-      updateClient.run( client.status, client.priority, client.id )
+      updateClient.run(client.status, client.priority, client.id)
     })
   }
-  
   updateAllClients(clients)
-  
+
   return res.status(200).send(clients);
 });
 
